f17218b597563c8f6dfb47ad158f98c15db60d2f
june01 <june_01@163.com>
2018-01-26 09:57:54 +0800

    update future api

--- src/controller/weather.js
+++ src/controller/weather.js
@@ -4,13 +4,13 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-const weatherArr = ['sunny', 'cloudy', 'overcast', 'lightrain', 'heavyrain', 'snow']
+const weatherArr = ['sunny', 'cloudy', 'overcast', 'lightrain', 'heavyrain', 'snow'];
 function randomWeather() {
     return weatherArr[random(0, 6)]
 }
 
 async function getNow(ctx, next) {
-    let forecast = []
+    let forecast = [];
     for (let i = 0; i < 24; i++) {
         forecast.push({
             temp: random(-10, 20),
@@ -28,7 +28,18 @@ async function getNow(ctx, next) {
 }
 
 async function getFuture(ctx, next) {
-    ctx.result = [];
+    let future = [];
+    let temp1 = random(-10, 20);
+    let temp2 = random(-10, 20);
+    for (let i = 0; i < 7; i++) {
+        forecast.push({
+            minTemp: Math.min(temp1, temp2),
+            maxTemp: Math.max(temp1, temp2),
+            weather: randomWeather(),
+        });
+    }
+
+    ctx.result = future;
 }
 
 module.exports = {
